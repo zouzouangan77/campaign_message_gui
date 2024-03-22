5695040cd43a7ede1600b76658ec83e1e81b30d0
zouzou77 <zouzou.angan@groupe-esigelec.org>
2024-03-22 17:35:16 +0100

    ajout de d'autre element

--- frontend/src/views/pages/group/GroupView.vue
+++ frontend/src/views/pages/group/GroupView.vue
@@ -4,6 +4,8 @@ import { ref, onMounted } from 'vue';
 const sortOrder = ref(1);
 const sortField = ref('');
 const sortKey = ref('');
+const goupDialog = ref(false);
+const mavalue = ref('');
 
 const picklistValue = ref([
     [
@@ -17,17 +19,22 @@ const picklistValue = ref([
     ],
     []
 ]);
+const openNew = () => {
+    goupDialog.value = true;
+};
+const hideDialog = () => {
+    goupDialog.value = false;
+   
+};
 
-const orderlistValue = ref([
-    { name: 'San Francisco', code: 'SF' },
+const selectedCity = ref();
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
     { name: 'London', code: 'LDN' },
-    { name: 'Paris', code: 'PRS' },
     { name: 'Istanbul', code: 'IST' },
-    { name: 'Berlin', code: 'BRL' },
-    { name: 'Barcelona', code: 'BRC' },
-    { name: 'Rome', code: 'RM' }
+    { name: 'Paris', code: 'PRS' }
 ]);
-
 
 const onSortChange = (event: MouseEvent) => {
     const value = (event.target as HTMLInputElement).value;
@@ -49,10 +56,29 @@ const onSortChange = (event: MouseEvent) => {
 
 <template>
     <div class="grid">
+        <!-- Colonne pour la liste des groupes -->
+        <div class="col-12 lg:col-4">
+            <!-- Card pour la liste des groupes -->
+            <div class="card">
+                <h5>LIST DES GROUPES</h5>
+                <!-- Listbox pour afficher les groupes -->
+                <div class="card flex justify-content-center">
+                    <Listbox v-model="selectedCity" :options="cities" filter optionLabel="name" class="w-full md:w-14rem" />
+                </div>
+                <!-- Boutons Ajouter et supprimer -->
+                <div>
+                    <Button label="Ajouter" icon="pi pi-times" severity="success" class="mr-2 mt-2 end" @click="openNew"/>
+                    <Button label="supprimer" icon="pi pi-check" severity="danger" @click="deleteContact" />
+                </div>
+            </div>
+        </div>
         
+        <!-- Colonne pour le picklist -->
         <div class="col-12 lg:col-8">
+            <!-- Card pour le picklist -->
             <div class="card">
                 <h5>Groupe selectioné</h5>
+                <!-- PickList pour afficher les groupes sélectionnés -->
                 <PickList v-model="picklistValue" listStyle="height:250px" dataKey="code">
                     <template #sourceheader> contacts </template>
                     <template #targetheader> contacts ajoutés </template>
@@ -60,19 +86,36 @@ const onSortChange = (event: MouseEvent) => {
                         <div>{{ slotProps.item.name }}</div>
                     </template>
                 </PickList>
-            </div>
-        </div>
-
-        <div class="col-12 lg:col-4">
-            <div class="card">
-                <h5>LIST DES GROUPES</h5>
-                <OrderList v-model="orderlistValue" listStyle="height:250px" dataKey="code" :rows="10">
-                    <template #header> Cities </template>
-                    <template #item="slotProps">
-                        <div>{{ slotProps.item.name }}</div>
-                    </template>
-                </OrderList>
+                <!-- Boutons valider et Annuler -->
+                <div>
+                    <Button label="valider" icon="pi pi-times" severity="success" class="mr-2 mt-2 end" @click="deleteContactDialog = false"/>
+                    <Button label="Annuler" icon="pi pi-check" severity="danger" @click="deleteContact" />
+                </div>
             </div>
         </div>
     </div>
+
+    <!-- Dialog pour ajouter un nouveau groupe -->
+    <Dialog v-model:visible="goupDialog" :style="{width: '450px'}" header="Groupe  Details" :modal="true" class="p-fluid">
+        <!-- Champ pour le titre du groupe -->
+        <div class="field">
+            <label for="name">Titre du groupe</label>
+            <InputText type="text" v-model="mavalue" />
+            <!-- Message d'erreur s'il n'y a pas de valeur saisie -->
+            <small class="p-error" v-if="submitted && !contact.firstName">FirstName is required.</small>
+        </div>
+  
+        <!-- Champ pour le commentaire du groupe -->
+        <div class="field">
+            <label for="content">commentaire du groupe</label>
+            <!-- Textarea pour saisir le commentaire -->
+            <Textarea v-model="mavalue" rows="5" cols="30" />
+        </div>
+     
+        <!-- Boutons Cancel et Save -->
+        <template #footer>
+            <Button label="Cancel" icon="pi pi-times" text @click="hideDialog"/>
+            <Button label="Save" icon="pi pi-check" text @click="saveContact" />
+        </template>
+    </Dialog>
 </template>
