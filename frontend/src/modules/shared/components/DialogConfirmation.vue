a455a137fd46af1d928b28c3fbb3e0d24c895904
zouzou77 <zouzou.angan@groupe-esigelec.org>
2024-03-26 16:10:44 +0100

    ajout d'element

--- frontend/src/modules/shared/components/DialogConfirmation.vue
+++ frontend/src/modules/shared/components/DialogConfirmation.vue
@@ -15,12 +15,12 @@ defineEmits<{
 <template>
   <Dialog :visible="visible" :style="{ width: '450px' }" header="Confirmation" modal>
     <div class="confirmation-content">
-      <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+      <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem; color: red;" />
       <span>{{ message }}</span>
     </div>
     <template #footer>
-      <Button label="No" icon="pi pi-times" text @click="visible = false" />
-      <Button label="Yes" icon="pi pi-check" text  @click="$emit('confirmation')"/>
+      <Button label="No" icon="pi pi-times" severity="danger" text @click="visible = false" />
+      <Button label="Yes" icon="pi pi-check" severity="success" text  @click="$emit('confirmation')"/>
     </template>
   </Dialog>
 </template>
