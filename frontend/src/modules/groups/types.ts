a13659cfb8ef1b56136e114aa0af28417eaac0a2
Tony TAFEUMEWE <t.tafeumewe@sgfgas.fr>
2024-03-26 15:28:58 +0100

    mise en place de l ajout des contacts, modification dans un group

--- frontend/src/modules/groups/types.ts
+++ frontend/src/modules/groups/types.ts
@@ -2,12 +2,18 @@ export interface IGroup {
    
     name: string;
     comment?: string;
+    addContacts?: Array<{id : number | undefined}>
+    removeContacts?: Array<{id : number | undefined}>
   }
   export class Group implements IGroup {
     constructor(
       public id?: number,
       public name: string = '',
       public comment?: string,
+      public addContacts?: Array<{id : number | undefined}>,
+      public removeContacts?: Array<{id : number | undefined}>,
     ) {}
   }
+
+
   
--- frontend/src/views/pages/group/GroupView.vue
+++ frontend/src/views/pages/group/GroupView.vue
@@ -14,37 +14,36 @@ import {
   findAllContactsByGroup
 
 } from '@/modules/contacts/contacts.api'
-import { ref, onMounted, computed } from 'vue';
+import {ref, onMounted, computed} from 'vue';
 import DialogGroup from './DialogGroup.vue';
 import DialogConfirmation from '../../../modules/shared/components/DialogConfirmation.vue'
-import { Group } from "@/modules/groups/types";
-import { useToast } from 'primevue/usetoast'
-import { Contact } from '@/modules/contacts/types';
-import { PickListMoveAllToSourceEvent, PickListMoveAllToTargetEvent, PickListMoveToSourceEvent, PickListMoveToTargetEvent } from 'primevue/picklist';
-
-
+import {Group} from "@/modules/groups/types";
+import {useToast} from 'primevue/usetoast'
+import {Contact} from '@/modules/contacts/types';
+import type {ListboxChangeEvent} from "primevue/listbox";
 
 
 const toast = useToast()
-const groupDialog = ref(false);
+const visibleGroupDialog = ref(false);
 const isNewGroup = ref(true);
 
 const group = ref(new Group());
 const groups = ref(new Array<Group>());
 const selectedGroup = ref(new Group());
-const deleteGroupDialog = ref(false);
+const visibleConfirmDeleteGroupDialog = ref(false);
 const allContacts = ref(new Array<Contact>());
 const contactsGroup = ref(new Array<Contact>());
-const addContacts = ref(new Array<{ id: number }>());
-const removeContacts = ref(new Array<{ id: number }>());
 
 const privateContacts = computed(() => {
   return allContacts.value.filter(contact => !contactsGroup.value.some(groupContact => groupContact.id === contact.id));
 });
-//const contactsByGroupSelected=ref(new Array<Contact>());
 
-const picklistContactValue = ref([new Array<Contact>(), new Array<Contact>()]);
+const picklistContacts = ref([new Array<Contact>(), new Array<Contact>()]);
+const canBeValidateOrCancel = computed(() => {
+  const [sourceContacts, targetContacts] = picklistContacts.value
+  return targetContacts.every(targetContact => contactsGroup.value.some((contact) => contact.id === targetContact.id))
 
+})
 onMounted(async () => {
   await updateDataList();
   await initDataPickList();
@@ -56,7 +55,7 @@ async function updateDataList() {
 }
 
 async function updateDataPickList() {
-  picklistContactValue.value = [privateContacts.value, contactsGroup.value];
+  picklistContacts.value = [privateContacts.value, contactsGroup.value];
 
 }
 
@@ -65,10 +64,12 @@ async function initDataPickList() {
 }
 
 const groupChanged = async () => {
-  contactsGroup.value = await findAllContactsByGroup(selectedGroup.value.id!)
-  updateDataPickList()
-  addContacts.value=[]
-  removeContacts.value=[]
+
+  if (selectedGroup != null && selectedGroup != undefined && selectedGroup.value != null && selectedGroup.value != undefined) {
+    contactsGroup.value = await findAllContactsByGroup(selectedGroup.value.id!)
+    updateDataPickList()
+  }
+
 }
 
 const cancelAddContact = async () => {
@@ -78,10 +79,9 @@ const cancelAddContact = async () => {
 
 const openNewGroup = () => {
   group.value = new Group()
-  groupDialog.value = true
+  visibleGroupDialog.value = true
   isNewGroup.value = true
 };
-
 
 
 const updateCreateGroup = async () => {
@@ -91,14 +91,14 @@ const updateCreateGroup = async () => {
       toast.add({
         severity: 'success',
         summary: 'Successful',
-        detail: 'Conctact created',
+        detail: 'Group created',
         life: 3000
       })
     } catch (error) {
       toast.add({
         severity: 'error',
         summary: 'Faillure',
-        detail: 'Conctact not created',
+        detail: 'Group not created',
         life: 3000
       })
     }
@@ -108,14 +108,14 @@ const updateCreateGroup = async () => {
       toast.add({
         severity: 'success',
         summary: 'Successful',
-        detail: 'Conctact updated',
+        detail: 'Group updated',
         life: 3000
       })
     } catch (error) {
       toast.add({
         severity: 'error',
         summary: 'Faillure',
-        detail: 'Conctact not created',
+        detail: 'Group not created',
         life: 3000
       })
     }
@@ -125,19 +125,26 @@ const updateCreateGroup = async () => {
 }
 
 const saveContactOnGroupe = async () => {
+  const [sourceContacts, targetContacts] = picklistContacts.value
+  const addContacts = targetContacts.filter((contact) => !contactsGroup.value.some(groupContact => groupContact.id === contact.id)).map(item => ({id: item.id}))
+  const removeContacts = contactsGroup.value.filter((contact) => sourceContacts.some(sourceContact => sourceContact.id === contact.id)).map(item => ({id: item.id}))
+  group.value = selectedGroup.value
+  group.value.addContacts = addContacts
+  group.value.removeContacts = removeContacts
+
   try {
     await updateGroupApi(group.value.id!, group.value)
     toast.add({
       severity: 'success',
       summary: 'Successful',
-      detail: 'Conctact updated',
+      detail: 'Group updated',
       life: 3000
     })
   } catch (error) {
     toast.add({
       severity: 'error',
       summary: 'Faillure',
-      detail: 'Conctact not created',
+      detail: 'Group not updated',
       life: 3000
     })
   }
@@ -146,72 +153,20 @@ const saveContactOnGroupe = async () => {
 
 const confirmDeleteGroup = (groupData: Group) => {
   group.value = groupData
-  deleteGroupDialog.value = true
+  visibleConfirmDeleteGroupDialog.value = true
 }
 
 const deleteGroup = async () => {
-  deleteGroupDialog.value = false
+  visibleConfirmDeleteGroupDialog.value = false
   await deleteGroupApi(group.value.id!)
-  toast.add({ severity: 'success', summary: 'Successful', detail: 'Conctact Deleted', life: 3000 })
+  toast.add({severity: 'success', summary: 'Successful', detail: 'Group Deleted', life: 3000})
   await updateDataList();
 }
 
 const editGroup = (updateGroup: Group) => {
   group.value = updateGroup
-  groupDialog.value = true
+  visibleGroupDialog.value = true
   isNewGroup.value = false
-}
-
-
-const moveToTaget = (event: PickListMoveToTargetEvent) => {
-  event.items.forEach((item: Contact) => {
-    addContacts.value.push({ id: item.id })
-  });
-
-  contactsGroup.value.forEach((contact: Contact) => {
-    addContacts.value = addContacts.value.filter(item => item.id !== contact.id)
-  });
-  console.log('addContacts = ', addContacts.value)
-  console.log('removeContacts = ', removeContacts.value)
-}
-
-const moveToSource = (event: PickListMoveToSourceEvent) => {
-  event.items.forEach((item: Contact) => {
-    removeContacts.value.push({ id: item.id })
-  });
-
-  contactsGroup.value.forEach((contact: Contact) => {
-    removeContacts.value = removeContacts.value.filter(item => item.id === contact.id)
-  });
-
-  console.log('addContacts = ', addContacts.value)
-  console.log('removeContacts = ', removeContacts.value)
-}
-
-const moveAllToSource = (event: PickListMoveAllToSourceEvent) => {
-  event.items.forEach((item: Contact) => {
-    removeContacts.value.push({ id: item.id })
-  });
-
-  contactsGroup.value.forEach((contact: Contact) => {
-    removeContacts.value = addCoremoveContactsntacts.value.filter(item => item.id === contact.id)
-  });
-  console.log('addContacts = ', addContacts.value)
-  console.log('removeContacts = ', removeContacts.value)
-
-}
-
-const moveAllToTarget = (event: PickListMoveAllToTargetEvent) => {
-  event.items.forEach((item: Contact) => {
-    addContacts.value.push({ id: item.id })
-  });
-
-  contactsGroup.value.forEach((contact: Contact) => {
-    addContacts.value = addContacts.value.filter(item => item.id !== contact.id)
-  });
-  console.log('addContacts = ', addContacts.value)
-  console.log('removeContacts = ', removeContacts.value)
-
 }
 
 </script>
@@ -226,25 +181,24 @@ const moveAllToTarget = (event: PickListMoveAllToTargetEvent) => {
 
         <div class="card flex justify-content-center">
           <Listbox v-model="selectedGroup" :options="groups" filter optionLabel="name" class="w-full md:w-25rem"
-            @change="groupChanged">
-            <template #option="{ option }">
+                   @change="groupChanged">
+            <template #option="slotProps">
               <div class="flex justify-content-between">
-                <div>{{ option.name }}</div>
+                <div>{{ slotProps.option.name }}</div>
                 <div>
                   <!-- Bouton pour modifier le groupe -->
-                  <Button icon="pi pi-pencil" rounded class="mr-1" @click="editGroup(option)" />
+                  <Button icon="pi pi-pencil" rounded class="mr-1" @click="editGroup(slotProps.option)"/>
                   <!-- Bouton pour supprimer le groupe -->
                   <Button icon="pi pi-trash" rounded severity="danger"
-                    @click="confirmDeleteGroup(option); selectedGroup = option" />
+                          @click="confirmDeleteGroup(slotProps.option)"/>
                 </div>
               </div>
             </template>
           </Listbox>
         </div>
         <div>
-          <Button label="ajouter" icon="pi pi-times" severity="success" class="mr-2 mt-2 end" @click="openNewGroup" />
+          <Button label="ajouter" icon="pi pi-times" severity="success" class="mr-2 mt-2 end" @click="openNewGroup"/>
         </div>
-
 
 
       </div>
@@ -255,13 +209,12 @@ const moveAllToTarget = (event: PickListMoveAllToTargetEvent) => {
 
       <div class="card">
 
-        <h5>Groupe selectioné {{ selectedGroup.name }}</h5>
+        <h5 v-if="!selectedGroup">Aucun groupe selectionné</h5>
+        <h5 v-else>Groupe selectioné {{ selectedGroup.name }}</h5>
         <!-- PickList pour afficher les groupes sélectionnés -->
-        <PickList v-model="picklistContactValue" listStyle="height:250px, weight:250px" dataKey="id"
-          @move-to-target="moveToTaget" @move-to-source="moveToSource" @move-all-to-target="moveAllToTarget"
-          @move-all-to-source="moveAllToSource">
-          <template #sourceheader> contacts </template>
-          <template #targetheader> contacts ajoutés </template>
+        <PickList v-model="picklistContacts" listStyle="height:250px, weight:250px" dataKey="id">
+          <template #sourceheader> contacts</template>
+          <template #targetheader> contacts ajoutés</template>
           <template #item="slotProps">
             <div class="flex flex-wrap p-2 align-items-center gap-3">
               <div class="flex-1 flex flex-column gap-2">
@@ -275,23 +228,25 @@ const moveAllToTarget = (event: PickListMoveAllToTargetEvent) => {
             </div>
 
 
-
           </template>
         </PickList>
         <!-- Boutons valider et Annuler -->
         <div>
           <Button label="valider" icon="pi pi-times" severity="success" class="mr-2 mt-2 end"
-            @click="saveContactOnGroupe" />
-          <Button label="Annuler" icon="pi pi-check" severity="danger" @click="cancelAddContact" />
+                  :disabled="canBeValidateOrCancel"
+                  @click="saveContactOnGroupe"/>
+          <Button label="Annuler" icon="pi pi-check" severity="danger" @click="cancelAddContact"
+                  :disabled="canBeValidateOrCancel"/>
         </div>
       </div>
     </div>
   </div>
 
-  <DialogGroup :group="group" v-model:visible="groupDialog" @valider="updateCreateGroup" />
+  <DialogGroup v-model:group="group" v-model:visible="visibleGroupDialog" @valider="updateCreateGroup"/>
 
-  <DialogConfirmation v-model:visible="deleteGroupDialog" message="Voulez vous vraiment supprimer ce group ?"
-    @confirmation="deleteGroup" />
+  <DialogConfirmation v-model:visible="visibleConfirmDeleteGroupDialog"
+                      message="Voulez vous vraiment supprimer ce group ?"
+                      @confirmation="deleteGroup"/>
 
 </template>
 
