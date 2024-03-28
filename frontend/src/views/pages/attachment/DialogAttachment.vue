92865b954921ea072035b4f0eac19b7e6c7b77c5
Tony TAFEUMEWE <t.tafeumewe@sgfgas.fr>
2024-03-28 10:53:13 +0100

    ajout librairie for backend

--- frontend/src/views/pages/attachment/DialogAttachment.vue
+++ frontend/src/views/pages/attachment/DialogAttachment.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineEmits, defineModel, ref } from 'vue'
+import { ref } from 'vue'
 import { Attachment } from '@/modules/attachments/types'
 import type { FileUploadUploaderEvent } from 'primevue/fileupload'
 import { useToast } from 'primevue/usetoast'
