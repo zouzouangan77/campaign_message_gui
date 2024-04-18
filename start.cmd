5cf7ed37feff57882c62774b8be2a7f7de6caa03
Tony TAFEUMEWE <t.tafeumewe@sgfgas.fr>
2024-04-18 22:58:33 +0200

    modification script start

--- start.cmd
+++ start.cmd
@@ -3,7 +3,7 @@
 rem Vérifier si Node.js est installé
 where node >nul 2>nul
 if %errorlevel% neq 0 (
-    echo Node.js n'est pas installé sur ce système.
+    echo "Node.js n'est pas installé sur ce système."
     set /p "install=Voulez-vous installer Node.js ? (O/N) :"
     if /i "%install%"=="O" (
         start https://nodejs.org/
@@ -19,7 +19,7 @@ rem Lancer le fichier main.js s'il existe
 if exist dist\main.js (
     node dist\main.js
 ) else (
-    echo Le fichier dist\main.js n'existe pas dans ce répertoire.
+    echo "Le fichier dist\main.js n'existe pas dans ce répertoire."
 )
 
 pause
